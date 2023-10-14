f13f1703c83affd433b0e9eb2b5a3f191f70bf09
eggmun98 <eggmun98@gmail.com>
2023-10-14 17:07:00 +0900

    [2023.10.14] Feat 소켓 연결 테스트

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,33 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 import cors from "cors";
 
 const app = express();
-
 const PORT = 4000;
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  // io.on("connection", ...)은 새로운 클라이언트가 연결될 때 마다 감지
+  console.log("Client connected");
+
+  socket.on("disconnect", () => {
+    // socket.on("disconnect", () => {}은 클라이언트 연결이 종료될 때 마다 작동
+    console.log("Client disconnected");
+  });
+});
 
 app.use(cors());
 app.use("/api/test", (req, res) => {
   console.log("작동");
   res.send("연결 성공");
 });
-const handleListening = () => console.log("서버 시작 테스트");
 
-app.listen(PORT, handleListening);
+const handleListening = () => console.log("서버 시작 테스트");
+httpServer.listen(PORT, handleListening);
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
 import axios from "axios";
+import { io } from "socket.io-client";
 
 export default function Home() {
+  useEffect(() => {
+    const socket = io("http://localhost:4000");
+    // 백엔드와 연결 시도
+
+    // 연결 시도 성공 시 아래 코드 작동
+    socket.on("connect", () => {
+      console.log("Connected to backend");
+    });
+
+    // useEffect 내에 cleanup 함수라고 한다.
+    // setInterval 함수의 clearInterval 함수와 같은 기능을 함
+    // cleanup 함수를 사용해야 메모리상에서 계속 소켓이 연결된 함수를
+    // 삭제 가능
+    //  socket.disconnect()는 소켓 연결 종료시키는 함수 즉 백엔드와 클라이언트 연결 종료
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   const test = async () => {
     try {
       const result = await axios("http://localhost:4000/api/test");
